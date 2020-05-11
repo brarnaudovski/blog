c669509f6428655f488cba9f7452535a83c6ae75
brarnaudovski <branko.a@slicelife.com>
2020-05-11 10:59:20 +0200

    Option to create an Article

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   post '/articles', to: 'articles#create'
   get '/articles/new', to: 'articles#new', as: :new_article
   get '/articles/:id', to: 'articles#show', as: :article
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
 
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
