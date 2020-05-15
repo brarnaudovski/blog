04a35cf47b19fe97c81028d556d948cbe6d2424b
brarnaudovski <branko.a@slicelife.com>
2020-05-15 18:48:27 +0200

    Use resources method for Rails in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  # get 'articles/index'
-  get '/articles', to: 'articles#index'
-  post '/articles', to: 'articles#create'
-  get '/articles/new', to: 'articles#new', as: :new_article
-  get '/articles/:id', to: 'articles#show', as: :article
-  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch '/articles/:id', to: 'articles#update'
-  delete '/articles/:id', to: 'articles#destroy'
-
   root to: 'articles#index'
+
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
