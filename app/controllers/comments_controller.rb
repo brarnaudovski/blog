1a969604a066f0776432ebac90b2b5b694bf82c4
brarnaudovski <branko.a@slicelife.com>
2020-06-08 07:38:20 +0200

    Comments Controller with actions for creating a new comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def new
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.build
+  end
+
+  def create
+    article = Article.find(params[:article_id])
+
+    comment = article.comments.build(comment_params)
+
+    comment.save
+    redirect_to article
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
