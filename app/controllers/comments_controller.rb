90b8201530286cd394926da7db99ea4518690eb7
Branko Arnaudovski <brarnaudovski@gmail.com>
2020-06-08 07:47:30 +0200

    Editing a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @comment = Comment.find(params[:id])
+    @article = @comment.article
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @article = @comment.article
+
+    if @comment.update(comment_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
   private
 
   def comment_params
