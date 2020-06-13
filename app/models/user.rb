34bef053078febbe6ba8be8efab416f14aa80ca5
brarnaudovski <branko.a@slicelife.com>
2020-06-13 18:20:55 +0200

    Add password attribute to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  has_secure_password
+
   before_save :email_to_downcase
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, length: { maximum: 50 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_174734) do
+ActiveRecord::Schema.define(version: 2020_06_13_153318) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_06_11_174734) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
