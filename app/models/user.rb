3e5162c463c20a1162175bcb8455dad36a571add
Igor Petrovski <ipetrovski3@gmail.com>
2020-06-17 11:14:34 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 50 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 
   private
 
