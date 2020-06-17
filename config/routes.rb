fefdca48c022008b3001deb34df32b38a52ebda1
Branko Arnaudovski <brarnaudovski@gmail.com>
2020-06-17 08:46:16 +0200

    New Users Controller with :new, :create and :show actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
