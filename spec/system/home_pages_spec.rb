9c1d4f56e3ce061ea0eaaa7b26c9dd8ec9bcdaf5
Branko Arnaudovski <brarnaudovski@gmail.com>
2020-07-09 20:58:38 +0200

    New system spec for Home Page

---
+++ spec/system/home_pages_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+
+RSpec.describe "HomePages" do
+  before do
+    driven_by :selenium, using: :chrome
+    # driven_by :rack_test
+
+    visit root_path
+  end
+
+  def log_in(user)
+    visit login_path
+
+    within('form') do
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+
+      click_on 'Log In'
+    end
+  end
+
+  it 'shows the home link' do
+    expecting = page.has_link?('My Blog')
+
+    expect(expecting).to be true
+  end
+
+  context 'when no user is sign in' do
+    it 'shows the Log In link' do
+      expecting = page.has_link?('Log In')
+
+      expect(expecting).to be true
+    end
+
+    it 'shows the Sign Up link' do
+      expecting = page.has_link?('Sign Up')
+
+      expect(expecting).to be true
+    end
+  end
+
+  context 'when user is sign in into the app' do
+    before do
+      log_in(create(:user))
+      visit root_path
+    end
+
+    it 'shows the New Article link' do
+      expecting = page.has_link?('New Article')
+
+      expect(expecting).to be true
+    end
+
+    it 'shows the Log Out link' do
+      expecting = page.has_link?('Log Out')
+
+      expect(expecting).to be true
+    end
+  end
+
+  context 'when an article is present' do
+    let!(:article) { create(:article, title: 'Testing with RSpec', body: 'Testing article body') }
+
+    before do
+      visit root_path
+    end
+
+    it 'shows the article title' do
+      expecting = page.has_content?(article.title)
+
+      expect(expecting).to be true
+    end
+
+    it 'shows the article body' do
+      expecting = page.has_content?(article.body)
+
+      expect(expecting).to be true
+    end
+
+    it 'shows the link to article' do
+      expecting = page.has_link?('Show')
+
+      expect(expecting).to be true
+    end
+  end
+end
