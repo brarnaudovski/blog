03656ea48bcd3037c90a436c0cb4e070866216e2
Branko Arnaudovski <brarnaudovski@gmail.com>
2020-08-06 20:53:54 +0200

    wip: localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   before_action :require_login
+  around_action :switch_locale
 
   private
 
@@ -10,5 +11,14 @@ class ApplicationController < ActionController::Base
       flash[:danger] = 'Please sign in to continue.'
       redirect_to login_path
     end
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
+  get '/:locale' => 'articles#index'
 
-  resources :articles do
-    resources :comments
+  scope "(:locale)", locale: /en|mk/ do
+    resources :articles do
+      resources :comments
+    end
+
+    get 'users/signup', to: 'users#new'
+    resources :users, except: :new
+
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
   end
-
-  get 'users/signup', to: 'users#new'
-  resources :users, except: :new
-
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
